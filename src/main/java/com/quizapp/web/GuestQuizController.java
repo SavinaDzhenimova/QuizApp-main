de930cb6c76353bc0b5a3a90ed79a28b68005cca
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-11-26 14:40:36 +0200

    Removed common methods from guest and user quiz controllers

--- src/main/java/com/quizapp/web/GuestQuizController.java
+++ src/main/java/com/quizapp/web/GuestQuizController.java
@@ -2,13 +2,11 @@ package com.quizapp.web;
 
 import com.quizapp.model.dto.quiz.QuizDTO;
 import com.quizapp.model.dto.quiz.QuizResultDTO;
-import com.quizapp.model.entity.Quiz;
 import com.quizapp.service.interfaces.GuestQuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
@@ -18,30 +16,6 @@ import java.util.Map;
 public class GuestQuizController {
 
     private final GuestQuizService guestQuizService;
-
-    @PostMapping("/start")
-    public String createQuiz(@RequestParam("categoryId") Long categoryId,
-                             RedirectAttributes redirectAttributes) {
-
-        int numberOfQuestions = 5;
-
-        Quiz quiz = this.guestQuizService.createQuiz(categoryId, numberOfQuestions);
-
-        redirectAttributes.addAttribute("page", 0);
-
-        return "redirect:/guest/quizzes/" + quiz.getViewToken();
-    }
-
-    @GetMapping("/{viewToken}")
-    public ModelAndView showQuiz(@PathVariable String viewToken) {
-        ModelAndView modelAndView = new ModelAndView("quiz");
-
-        Quiz quiz = this.guestQuizService.getQuizFromTemp(viewToken);
-        modelAndView.addObject("quiz", quiz);
-        modelAndView.addObject("isLogged", false);
-
-        return modelAndView;
-    }
 
     @PostMapping("/{viewToken}/submit")
     public String submitQuiz(@PathVariable String viewToken, @RequestParam Map<String, String> formData) {
--- src/main/java/com/quizapp/web/UserQuizController.java
+++ src/main/java/com/quizapp/web/UserQuizController.java
@@ -2,7 +2,6 @@ package com.quizapp.web;
 
 import com.quizapp.model.dto.quiz.QuizDTO;
 import com.quizapp.model.dto.quiz.QuizResultDTO;
-import com.quizapp.model.entity.Quiz;
 import com.quizapp.service.interfaces.UserQuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
@@ -20,30 +18,6 @@ import java.util.Map;
 public class UserQuizController {
 
     private final UserQuizService userQuizService;
-
-    @PostMapping("/start")
-    public String createQuiz(@RequestParam("categoryId") Long categoryId,
-                             RedirectAttributes redirectAttributes) {
-        int numberOfQuestions = 5;
-
-        Quiz quiz = this.userQuizService.createQuiz(categoryId, numberOfQuestions);
-
-        redirectAttributes.addAttribute("page", 0);
-
-        return "redirect:/users/quizzes/" + quiz.getViewToken();
-    }
-
-    @GetMapping("/{viewToken}")
-    public ModelAndView showQuiz(@PathVariable String viewToken) {
-
-        ModelAndView modelAndView = new ModelAndView("quiz");
-
-        Quiz quiz = this.userQuizService.getQuizFromTemp(viewToken);
-        modelAndView.addObject("quiz", quiz);
-        modelAndView.addObject("isLogged", true);
-
-        return modelAndView;
-    }
 
     @PostMapping("/{viewToken}/submit")
     public String submitQuiz(@PathVariable String viewToken,
