5a18c1172bed9d9f9c58a22d642ba600eb24a301
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-11-28 11:03:56 +0200

    Add category statistics entity

---
+++ src/main/java/com/quizapp/model/entity/CategoryStatistics.java
@@ -0,0 +1,32 @@
+package com.quizapp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category_statistics")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryStatistics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_id", nullable = false, unique = true)
+    private Long categoryId;
+
+    @Column(name = "category_name", nullable = false, unique = true)
+    private String categoryName;
+
+    private int totalSolvedQuizzes;
+
+    private double averageScore;
+
+    private double averageAccuracy;
+}
