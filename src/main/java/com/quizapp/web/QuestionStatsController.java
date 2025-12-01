eb42c61efb89cfe3b9d57af372908e3039cc5c30
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-12-01 19:25:19 +0200

    Add question statistics controller method

---
+++ src/main/java/com/quizapp/web/QuestionStatsController.java
@@ -0,0 +1,42 @@
+package com.quizapp.web;
+
+import com.quizapp.model.dto.question.QuestionStatsDTO;
+import com.quizapp.service.interfaces.QuestionStatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/statistics/questions")
+@RequiredArgsConstructor
+public class QuestionStatsController {
+
+    private final QuestionStatisticsService questionStatisticsService;
+
+    @GetMapping
+    public ModelAndView showQuestionsStats(@RequestParam(defaultValue = "0") int page,
+                                           @RequestParam(defaultValue = "10") int size,
+                                           @RequestParam(value = "categoryId", required = false) Long categoryId) {
+        ModelAndView modelAndView = new ModelAndView("questions-dashboard");
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by("questionId"));
+        Page<QuestionStatsDTO> questionStatsDTOs = this.questionStatisticsService
+                .getAllQuestionStatsForChartsByCategoryId(categoryId, pageable);
+
+        modelAndView.addObject("questionStats", questionStatsDTOs.getContent());
+        modelAndView.addObject("currentPage", questionStatsDTOs.getNumber());
+        modelAndView.addObject("totalPages", questionStatsDTOs.getTotalPages());
+        modelAndView.addObject("totalElements", questionStatsDTOs.getTotalElements());
+        modelAndView.addObject("size", questionStatsDTOs.getSize());
+        modelAndView.addObject("categoryId", categoryId);
+
+        return modelAndView;
+    }
+}
