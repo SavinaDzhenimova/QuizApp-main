f4a323876dd4200154e619c0bd291afb7813443f
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-12-12 22:42:37 +0200

    Added tests for show quiz and add submission dto is not present in quiz common service

--- src/test/java/com/quizapp/web/QuizCommonControllerTest.java
+++ src/test/java/com/quizapp/web/QuizCommonControllerTest.java
@@ -2,6 +2,8 @@ package com.quizapp.web;
 
 import com.quizapp.config.SecurityConfig;
 import com.quizapp.exception.CategoryNotFoundException;
+import com.quizapp.model.dto.quiz.QuizSubmissionDTO;
+import com.quizapp.model.dto.user.UserDetailsDTO;
 import com.quizapp.model.entity.Quiz;
 import com.quizapp.service.QuizCommonService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,13 +11,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Set;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,9 +40,18 @@ public class QuizCommonControllerTest {
     private GlobalController globalController;
 
     private Quiz quiz;
+    private UserDetailsDTO loggedUser;
 
     @BeforeEach
     void setUp() {
+        this.loggedUser = UserDetailsDTO.builder()
+                .id(1L)
+                .username("logged")
+                .email("logged@gmail.com")
+                .password("Password123")
+                .authorities(Set.of(new SimpleGrantedAuthority("ROLE_USER")))
+                .build();
+
         this.quiz = Quiz.builder()
                 .viewToken("token123")
                 .build();
@@ -80,5 +96,53 @@ public class QuizCommonControllerTest {
                     .with(csrf())
                     .param("categoryId", "1"))
                 .andExpect(status().isForbidden());
+    }
+
+    @WithAnonymousUser
+    @Test
+    void showQuiz_ShouldReturnQuizAndAddSubmissionDTO_WhenNotPresent() throws Exception {
+        when(this.quizCommonService.getQuizFromTemp("token123"))
+                .thenReturn(this.quiz);
+
+        this.mockMvc.perform(get("/quizzes/quiz/token123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("quiz"))
+                .andExpect(model().attributeExists("quizSubmissionDTO"))
+                .andExpect(model().attributeExists("quiz"))
+                .andExpect(model().attribute("quiz", this.quiz))
+                .andExpect(model().attributeExists("isLogged"))
+                .andExpect(model().attribute("isLogged", false));
+
+        verify(this.quizCommonService, times(1)).getQuizFromTemp("token123");
+    }
+
+    @Test
+    void showQuiz_ShouldNotOverrideQuizSubmissionDTO_WhenPresent() throws Exception {
+        QuizSubmissionDTO quizSubmissionDTO = QuizSubmissionDTO.builder().viewToken("token123").build();
+
+        when(this.quizCommonService.getQuizFromTemp("token123"))
+                .thenReturn(this.quiz);
+
+        this.mockMvc.perform(get("/quizzes/quiz/token123")
+                    .with(user(this.loggedUser)))
+                .andExpect(status().isOk())
+                .andExpect(view().name("quiz"))
+                .andExpect(model().attributeExists("quiz"))
+                .andExpect(model().attribute("quiz", this.quiz))
+                .andExpect(model().attributeExists("quizSubmissionDTO"))
+                .andExpect(model().attribute("quizSubmissionDTO", quizSubmissionDTO))
+                .andExpect(model().attributeExists("isLogged"))
+                .andExpect(model().attribute("isLogged", true));
+
+        verify(this.quizCommonService, times(1)).getQuizFromTemp("token123");
+    }
+
+    @WithMockUser(authorities = {"ROLE_ADMIN"})
+    @Test
+    void showQuiz_ShouldReturnError_WhenAdmin() throws Exception {
+        this.mockMvc.perform(get("/quizzes/quiz/token123"))
+                .andExpect(status().isForbidden());
+
+        verify(this.quizCommonService, never()).getQuizFromTemp(anyString());
     }
 }
