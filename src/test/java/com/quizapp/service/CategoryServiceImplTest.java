2f724c30c19a885149e1b5e76b11df563292c8ac
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-12-17 16:53:31 +0200

    Added tests for get category by id method in category service

--- src/test/java/com/quizapp/service/CategoryServiceImplTest.java
+++ src/test/java/com/quizapp/service/CategoryServiceImplTest.java
@@ -4,12 +4,17 @@ import com.quizapp.model.dto.category.CategoryDTO;
 import com.quizapp.model.dto.category.CategoryPageDTO;
 import com.quizapp.model.rest.CategoryApiDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
@@ -40,24 +45,29 @@ public class CategoryServiceImplTest {
     @Mock
     private RestClient.RequestBodySpec bodySpec;
 
-    @Test
-    void getAllCategories_ShouldReturnMappedDTOs() {
+    private CategoryApiDTO api1;
+    private CategoryApiDTO api2;
 
-        CategoryApiDTO api1 = CategoryApiDTO.builder()
+    @BeforeEach
+    void setUp() {
+        this.api1 = CategoryApiDTO.builder()
                 .id(1L)
                 .name("Maths")
                 .description("Description")
                 .questions(new ArrayList<>())
                 .build();
-        CategoryApiDTO api2 = CategoryApiDTO.builder()
+        this.api2 = CategoryApiDTO.builder()
                 .id(2L)
                 .name("Music")
                 .description("Description")
                 .questions(new ArrayList<>())
                 .build();
+    }
 
+    @Test
+    void getAllCategories_ShouldReturnMappedDTOs() {
         CategoryPageDTO<CategoryApiDTO> apiPage = new CategoryPageDTO<>();
-        apiPage.setCategories(List.of(api1, api2));
+        apiPage.setCategories(List.of(this.api1, this.api2));
         apiPage.setTotalPages(1);
         apiPage.setTotalElements(2);
         apiPage.setCurrentPage(0);
@@ -85,5 +95,36 @@ public class CategoryServiceImplTest {
         Assertions.assertEquals(2, result.getTotalElements());
         Assertions.assertEquals(0, result.getCurrentPage());
         Assertions.assertEquals(10, result.getSize());
+    }
+
+    @Test
+    void getCategoryById_ShouldReturnCategoryDTO_WhenCategoryFound() {
+        when(this.restClient.get()).thenReturn(this.getSpec);
+        when(this.getSpec.uri(eq("/api/categories/{id}"), eq(1L)))
+                .thenReturn(this.headersSpec);
+        when(this.headersSpec.retrieve()).thenReturn(this.responseSpec);
+        when(this.responseSpec.body(CategoryApiDTO.class)).thenReturn(this.api1);
+
+        CategoryDTO result = this.categoryService.getCategoryById(1L);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1L, result.getId());
+        Assertions.assertEquals("Maths", result.getName());
+        Assertions.assertEquals("Description", result.getDescription());
+    }
+
+    @Test
+    void getCategoryById_ShouldThrowException_WhenCategoryNotFound() {
+        when(this.restClient.get()).thenReturn(this.getSpec);
+        when(this.getSpec.uri(eq("/api/categories/{id}"), eq(1L)))
+                .thenReturn(this.headersSpec);
+        when(this.headersSpec.retrieve()).thenReturn(this.responseSpec);
+        when(this.responseSpec.body(CategoryApiDTO.class))
+                .thenThrow(HttpClientErrorException.NotFound
+                        .create(HttpStatus.NOT_FOUND, "Not Found", HttpHeaders.EMPTY, null, null));
+
+        CategoryDTO result = this.categoryService.getCategoryById(1L);
+
+        Assertions.assertNull(result);
     }
 }
